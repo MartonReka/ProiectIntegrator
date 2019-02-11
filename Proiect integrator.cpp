e7c4435ded6e4fffa94aafaf9c738a52cd09bd10
Marton Reka <rmarton910@gmail.com>
2019-02-11 23:57:59 +0200

    extract validation in separate metode

--- Proiect integrator.cpp
+++ Proiect integrator.cpp
@@ -1,6 +1,25 @@
 #include<iostream>
 using namespace std;
 
+bool isValid(char codDat[]) {
+	
+	int lungime = strlen(codDat);
+	cout << lungime << endl;
+	if (lungime > 9) {
+		cout << "Codul trebuie sa aiba max 9 caractere!!!" << endl;
+		return false;
+	}
+	int i = 0;
+	while (codDat[i] >= '0' && codDat[i] <= '9') {
+		i++;
+	}
+	if (i != lungime) {
+		cout << "Cod eronat: " << codDat << endl;
+		return false;
+	}
+	return true;
+}
+
 int main()
 {	
 	char codDat[10], denumire[21];
@@ -8,23 +27,13 @@ int main()
 	cin >> denumire;
 	cout << "Introduceti codul produsului (max 9 cifre, fara spatii): " << endl;
 	cin >> codDat;
-
-	int lungime = strlen(codDat);
-	cout << lungime << endl;
-	if (lungime > 9) {
-		cout << "Codul trebuie sa aiba max 9 caractere!!!" << endl;
+	
+	if (!isValid(codDat)) {
 		system("pause");
 		return 0;
-	}
-	int i = 0;
-	while (codDat[i] >= '0' && codDat[i] <= '9'){
-		i++;
-	}
-	if (i != lungime) {
-		cout << "Cod eronat: " << codDat << endl;
-		system("pause");
-		return 0;
-	}
 		
+	}
+	cout << "majd folytatjuk.puszipa" << endl;
+			
 	system("pause");
 }
